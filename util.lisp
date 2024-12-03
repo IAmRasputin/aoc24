a7bbbb81d311675e3065a5c89a708a29697374a4
Ryan Gannon <ryanmgannon@gmail.com>
2024-12-02 19:31:44 -0500

    Actually use the input-getter I've been writing for a month

--- util.lisp
+++ util.lisp
@@ -3,6 +3,4 @@
 (in-package #:aoc24/utils)
 
 (defun input (day)
-  (uiop:read-file-string (asdf:system-relative-pathname :aoc24 
-                                                        (format nil "input/~d" day))))
-
+  (advent/input:input 2024 day))
